2b6431e4ae7da6888906722a5ab7fbbc26e9fec5
Drew Keat <thedrewkeat@gmail.com>
2022-01-03 22:14:26 -0700

    commit tree ad61e97f4ef1278abdaad05186cb8eeac9537f8d

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+  buildReviews = () => {
+    const reviews = this.props.reviews.filter(review => (review.restaurantId === this.props.restaurantId))
+   return reviews.map(review => (
+     <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
+   ))
+  }
+
   render() {
     return (
       <ul>
         Reviews
+        {this.buildReviews()}
       </ul>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
-import ReviewInput from '../components/reviews/ReviewInput';
-import Reviews from '../components/reviews/Reviews';
+import React, { Component } from "react";
+import ReviewInput from "../components/reviews/ReviewInput";
+import Reviews from "../components/reviews/Reviews";
+import { addReview, deleteReview } from "../actions/reviews";
+import { connect } from "react-redux";
 
 class ReviewsContainer extends Component {
-
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput
+          restaurantId={this.props.restaurant.id}
+          addReview={this.props.addReview}
+        />
+        <Reviews
+        restaurantId={this.props.restaurant.id} 
+        reviews={this.props.reviews}
+        deleteReview={this.props.deleteReview}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default ReviewsContainer;
+export default connect(state => ({reviews: state.reviews}), { addReview, deleteReview })(ReviewsContainer);
